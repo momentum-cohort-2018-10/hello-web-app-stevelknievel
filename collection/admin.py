accee6e6cd338cb16e8cfa58311abbf3ebf59889
stevelknievel <marks.steven@gmail.com>
2018-11-15 00:19:01 -0500

    Thurs evening homework: Chpts 6-7

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+
 # Register your models here.
+from collection.models import VoiceOverEquipment
+
+
+class VoiceOverEquipmentAdmin(admin.ModelAdmin):
+    model = VoiceOverEquipment
+    list_display = ('name', 'description',)
+    prepopulated_fields = {'slug': ('name', )}
+
+
+admin.site.register(VoiceOverEquipment, VoiceOverEquipmentAdmin)
--- collection/views.py
+++ collection/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
+from collection.models import VoiceOverEquipment
 
 
 # Create your views here.
 def index(request):
     # defining the variable
-    number = 6
-    thing = "Thing name"
+    VoiceOverEquipments = VoiceOverEquipment.objects.all()
     # this is your new view.
     return render(request, 'index.html', {
-        'number': number,
-        'thing': thing,
+        'VoiceOverEquipments': VoiceOverEquipments,
     })
 
