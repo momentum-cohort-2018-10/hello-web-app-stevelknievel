3e94877532943a1ee5645b99d26054cc941be7ac
stevelknievel <marks.steven@gmail.com>
2018-11-15 14:22:41 -0500

    all changes to collection file through chpt 9

--- collection/models.py
+++ collection/models.py
@@ -6,4 +6,3 @@ class VoiceOverEquipment(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     slug = models.SlugField(unique=True)
-
--- collection/views.py
+++ collection/views.py
@@ -1,13 +1,47 @@
 from django.shortcuts import render
+from collection.forms import VoiceOverEquipmentForm
 from collection.models import VoiceOverEquipment
 
 
 # Create your views here.
 def index(request):
     # defining the variable
-    VoiceOverEquipments = VoiceOverEquipment.objects.all()
+    voiceoverequipments = VoiceOverEquipment.objects.all()
     # this is your new view.
     return render(request, 'index.html', {
-        'VoiceOverEquipments': VoiceOverEquipments,
+        'voiceoverequipments': voiceoverequipments,
     })
 
+
+def voiceoverequipment_detail(request, slug):
+    # grab the object...
+    voiceoverequipment = VoiceOverEquipment.objects.get(slug=slug)
+    # and pass to the template
+    return render(request, 'voiceoverequipments/voiceoverequipment_detail.html', {
+        'voiceoverequipment': voiceoverequipment,
+    })
+
+
+def edit_voiceoverequipment(request, slug):
+    # grab the object...
+    voiceoverequipment = VoiceOverEquipment.objects.get(slug=slug)
+    # set the form we're using...
+    form_class = VoiceOverEquipmentForm
+    # if we're coming to this view from a submitted form,
+    if request.method == 'POST':
+        # grab the data from the submitted form
+        form = form_class(data=request.POST, instance=voiceoverequipment)
+        if form.is_valid():
+            # save the new data
+            form.save()
+            return redirect('voiceoverequipment_detail', slug=voiceoverequipment.slug)
+
+    # otherwise just create the form
+    else:
+        form = form_class(instance=voiceoverequipment)
+
+    # and render the template
+    return render(request, 'voiceoverequipments/edit_voiceoverequipment.html', {
+        'voiceoverequipment': voiceoverequipment,
+        'form': form,
+    })
