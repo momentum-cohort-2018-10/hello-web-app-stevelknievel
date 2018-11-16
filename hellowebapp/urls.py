548fc05a934442f9880cef1212a7cc2db0544ce4
stevelknievel <marks.steven@gmail.com>
2018-11-16 13:56:36 -0500

    work up through chpt 10

--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
+from django.contrib.auth.views import (PasswordResetView, PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView,
+)
+from django.urls import path, include
 from django.contrib import admin
 from django.views.generic import TemplateView
 from collection import views
+
+
 
 urlpatterns = [
     path('', views.index, name='home'),
@@ -24,5 +28,10 @@ urlpatterns = [
     path('contact/', TemplateView.as_view(template_name='contact.html'), name='contact'),
     path('voiceoverequipments/<slug>/', views.voiceoverequipment_detail, name='voiceoverequipment_detail'),
     path('voiceoverequipments/<slug>/edit/', views.edit_voiceoverequipment, name='edit_voiceoverequipment'),
+    path('accounts/password/reset/', PasswordResetView.as_view(template_name='registration/password_reset_form.html'), name="password_reset"), 
+    path('accounts/password/reset/done/', PasswordResetView.as_view(template_name='registration/password_reset_done.html'), name="password_reset_done"),
+    path('accounts/password/reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name="password_reset_confirm"),
+    path('accounts/password/done/', PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name="password_reset_complete"),
+    path('accounts/', include('registration.backends.simple.urls')),
     path('admin/', admin.site.urls),
 ]
